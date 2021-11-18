238c7027a4edac1c5564f19006763a177754e718
rdinnager <r.dinnage@gmail.com>
2021-11-18 16:01:25 -0500

    added missing stub for cpu implementation of filtered_lrelu

--- csrc/src/filtered_lrelu_cpu.cpp
+++ csrc/src/filtered_lrelu_cpu.cpp
@@ -11,3 +11,10 @@ STYLEGANR_API void * c_styleganr_filtered_lrelu_act (void* x, void* si, int sx, 
   throw std::runtime_error("C++ `filtered_lrelu_act` is only supported on CUDA runtimes.");
   //LANTERN_FUNCTION_END
 }
+
+STYLEGANR_API void * c_styleganr_filtered_lrelu (void* x, void* fu, void* fd, void* b, void* si, int up, int down, int px0, int px1, int py0, int py1, int sx, int sy, float gain, float slope, float clamp, bool flip_filters, bool writeSigns)
+{
+  //LANTERN_FUNCTION_START
+  throw std::runtime_error("C++ `filtered_lrelu` is only supported on CUDA runtimes.");
+  //LANTERN_FUNCTION_END
+}
